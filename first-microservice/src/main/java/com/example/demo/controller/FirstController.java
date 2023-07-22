1e1e8e0412bf9c1e807f90a3a82e0e877d1364ef
Kishoredarne <kishore.darne@gmail.com>
2023-07-22 13:17:30 +0530

    update code

--- first-microservice/src/main/java/com/example/demo/controller/FirstController.java
+++ first-microservice/src/main/java/com/example/demo/controller/FirstController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/firstservice")
 public class FirstController {
 
-	@GetMapping("getmessage")
+	@GetMapping("/getmessage")
 	public String getMessage() {
 		return "Good morning from First microservice";
 	}
--- second-microservice/src/main/java/com/example/demo/controller/SecondController.java
+++ second-microservice/src/main/java/com/example/demo/controller/SecondController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/secondservice")
 public class SecondController {
 
-	@GetMapping("message")
+	@GetMapping("")
 	public String getMessage() {
 		return "thanks for called, I am Second Microservice";
 	}
